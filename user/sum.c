b3b68a0ab2be123b85188472eeffc5359ca7de3d
root <root@MSI>
2024-02-24 14:25:36 +0300

    Сделал пункт 1.c

--- user/sum.c
+++ user/sum.c
@@ -5,12 +5,15 @@
 void error() {
   printf("Должны быть введены два целых положительных числа, не превосходящих 2147483647\n");
   exit(1);
-}
+}  
 
 int main(int argc, char *argv[])
 {
   char s[22];
-  gets(s, 22);
+  int n = 0;
+  while(n < 22 && read(0, s + (n++), 1) > 0) { // Читаем по одному байту
+  	if (s[n - 1] == '\n') break;
+  }
   int i = 0, j;
   while (i < 22 && s[i] != ' ' && s[i] != '\n') ++i;
   if (i == 22 || s[i] == '\n') error();
